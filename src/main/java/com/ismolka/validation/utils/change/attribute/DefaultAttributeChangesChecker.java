28590ba22c0b91b5e48ab213dfb7e2a28757832b
igor.smolka <ihar_smolka@senla.eu>
2024-08-28 23:45:29 +0300

    черновой набросок

--- src/main/java/com/ismolka/validation/utils/change/attribute/DefaultAttributeChangesChecker.java
+++ src/main/java/com/ismolka/validation/utils/change/attribute/DefaultAttributeChangesChecker.java
@@ -47,18 +47,10 @@ public class DefaultAttributeChangesChecker<T> implements AttributeChangesChecke
             Object newAttrVal = attributeToCheck.fieldPath().getValueFromObject(newObj);
             Object oldAttrVal = attributeToCheck.fieldPath().getValueFromObject(oldObj);
 
-            if (attributeToCheck.changesChecker() != null) {
-                checkWithInnerCheckerAndPutInMapIfHasDiff(attributeToCheck, newAttrVal, oldAttrVal, attributeToCheck.changesChecker(), diffMap, sourceClass);
+            checkWithInnerCheckerAndPutInMapIfHasDiff(attributeToCheck, newAttrVal, oldAttrVal, attributeToCheck.changesChecker(), diffMap, sourceClass);
 
-                if (!diffMap.isEmpty() && stopOnFirstDiff) {
-                    break;
-                }
-            } else if (!isEquals(newAttrVal, oldAttrVal, attributeToCheck)) {
-               diffMap.put(attributeToCheck.fieldPath().path(), new AttributeDifference(attributeToCheck.fieldPath().path(), sourceClass, attributeToCheck.fieldPath().clazz(), oldAttrVal, newAttrVal));
-
-               if (stopOnFirstDiff) {
-                   break;
-               }
+            if (!diffMap.isEmpty() && stopOnFirstDiff) {
+                break;
             }
         }
 
@@ -75,56 +67,62 @@ public class DefaultAttributeChangesChecker<T> implements AttributeChangesChecke
             return;
         }
 
-        if (CollectionChangesChecker.class.isAssignableFrom(changesChecker.getClass())) {
-            if (!Collection.class.isAssignableFrom(newAttrVal.getClass()) || !Collection.class.isAssignableFrom(oldAttrVal.getClass())) {
-                throw new RuntimeException("One of objects is not a collection");
+        if (changesChecker != null) {
+            if (CollectionChangesChecker.class.isAssignableFrom(changesChecker.getClass())) {
+                if (!Collection.class.isAssignableFrom(newAttrVal.getClass()) || !Collection.class.isAssignableFrom(oldAttrVal.getClass())) {
+                    throw new RuntimeException("One of objects is not a collection");
+                }
+
+                CollectionChangesChecker collectionChangesChecker = (CollectionChangesChecker) changesChecker;
+                CollectionChangesCheckerResult collectionChangesCheckerResult = collectionChangesChecker.getResult((Collection) oldAttrVal, (Collection) newAttrVal);
+                diffMap.put(attributeToCheck.fieldPath().path(), collectionChangesCheckerResult);
+                return;
             }
 
-            CollectionChangesChecker collectionChangesChecker = (CollectionChangesChecker) changesChecker;
-            CollectionChangesCheckerResult collectionChangesCheckerResult = collectionChangesChecker.getResult((Collection) oldAttrVal, (Collection) newAttrVal);
-            diffMap.put(attributeToCheck.fieldPath().path(), collectionChangesCheckerResult);
+            if (AttributeChangesChecker.class.isAssignableFrom(changesChecker.getClass())) {
+                AttributeChangesChecker attributeChangesChecker = (AttributeChangesChecker) changesChecker;
+                AttributeChangesCheckerResult attributeChangesCheckerResult = attributeChangesChecker.getResult(oldAttrVal, newAttrVal);
+                diffMap.put(attributeToCheck.fieldPath().path(), attributeChangesCheckerResult);
+                return;
+            }
         }
 
-        if (AttributeChangesChecker.class.isAssignableFrom(changesChecker.getClass())) {
-            AttributeChangesChecker attributeChangesChecker = (AttributeChangesChecker) changesChecker;
-            AttributeChangesCheckerResult attributeChangesCheckerResult = attributeChangesChecker.getResult(oldAttrVal, newAttrVal);
-            diffMap.put(attributeToCheck.fieldPath().path(), attributeChangesCheckerResult);
-        }
-    }
-
-    private boolean isEquals(Object newAttrVal, Object oldAttrVal, AttributeMetaInfo attributeToCheck) {
-        if (newAttrVal == oldAttrVal) {
-            return true;
-        }
 
         if (attributeToCheck.equalsMethodReflectionRef() != null) {
-            Object firstNonNull = Stream.of(newAttrVal, oldAttrVal).filter(Objects::nonNull).findFirst().get();
-            Object argObj = Stream.of(newAttrVal, oldAttrVal).filter(obj -> obj != firstNonNull).findFirst().get();
+            Boolean result = (Boolean) ReflectionUtils.invokeMethod(attributeToCheck.equalsMethodReflectionRef(), oldAttrVal, newAttrVal);
 
-            Boolean result = (Boolean) ReflectionUtils.invokeMethod(attributeToCheck.equalsMethodReflectionRef(), firstNonNull, argObj);
+            if (result != null && result) {
+                diffMap.put(attributeToCheck.fieldPath().path(), new AttributeDifference(attributeToCheck.fieldPath().path(), sourceClass, attributeToCheck.fieldPath().clazz(), oldAttrVal, newAttrVal));
+            }
 
-            return result != null && result;
+            return;
         }
 
         if (!CollectionUtils.isEmpty(attributeToCheck.equalsFields())) {
-            //todo заполнять???
             for (FieldPath equalsField : attributeToCheck.equalsFields()) {
+                Class<?> attributeClass = equalsField.getLast().clazz();
                 Object oldObjVal = equalsField.getValueFromObject(oldAttrVal);
                 Object newObjVal = equalsField.getValueFromObject(newAttrVal);
 
                 if (!Objects.equals(oldObjVal, newObjVal)) {
-                    return false;
+                    diffMap.put(attributeToCheck.fieldPath().path(), new AttributeDifference(equalsField.path(), sourceClass, attributeClass, oldObjVal, newObjVal));
                 }
             }
 
-            return true;
+            return;
         }
 
         if (attributeToCheck.biEqualsMethodCodeRef() != null) {
-            return attributeToCheck.biEqualsMethodCodeRef().test(oldAttrVal, newAttrVal);
+            boolean biEqualsResult = attributeToCheck.biEqualsMethodCodeRef().test(oldAttrVal, newAttrVal);
+            if (!biEqualsResult) {
+                diffMap.put(attributeToCheck.fieldPath().path(), new AttributeDifference(attributeToCheck.fieldPath().path(), sourceClass, attributeToCheck.fieldPath().clazz(), oldAttrVal, newAttrVal));
+            }
         }
 
-        return Objects.equals(newAttrVal, oldAttrVal);
+        boolean equalsResult = Objects.equals(newAttrVal, oldAttrVal);
+        if (!equalsResult) {
+            diffMap.put(attributeToCheck.fieldPath().path(), new AttributeDifference(attributeToCheck.fieldPath().path(), sourceClass, attributeToCheck.fieldPath().clazz(), oldAttrVal, newAttrVal));
+        }
     }
 
     public static class Builder<T> {
