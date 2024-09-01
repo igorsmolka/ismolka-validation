e7ed493e3e1a19acb4e31c8b41e91dd814b09853
igor.smolka <ihar_smolka@senla.eu>
2024-09-02 00:16:37 +0300

    черновой набросок

--- src/main/java/com/ismolka/validation/utils/constant/CollectionOperation.java
+++ src/main/java/com/ismolka/validation/utils/constant/CollectionOperation.java
@@ -1,10 +1,24 @@
 package com.ismolka.validation.utils.constant;
 
+/**
+ * Possible modifying operations for {@link java.util.Collection}.
+ *
+ * @author Ihar Smolka
+ */
 public enum CollectionOperation {
 
+    /**
+     * Add element
+     */
     ADD,
 
+    /**
+     * Remove element
+     */
     REMOVE,
 
+    /**
+     * Update element
+     */
     UPDATE
 }
--- src/main/java/com/ismolka/validation/utils/constant/MapOperation.java
+++ src/main/java/com/ismolka/validation/utils/constant/MapOperation.java
@@ -1,10 +1,24 @@
 package com.ismolka.validation.utils.constant;
 
+/**
+ * Possible modifying operations for {@link java.util.Map}.
+ *
+ * @author Ihar Smolka
+ */
 public enum MapOperation {
 
+    /**
+     * Add element
+     */
     PUT,
 
+    /**
+     * Remove element
+     */
     REMOVE,
 
+    /**
+     * Update element
+     */
     UPDATE
 }
